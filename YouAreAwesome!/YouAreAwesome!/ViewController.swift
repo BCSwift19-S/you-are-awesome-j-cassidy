fc2a50e905e46a18978e63d52e4ac8237735d7dc
Jimmy Cassidy <jimmycassidy@Jimmys-MacBook-Air.local>
2019-01-23 02:30:42 -0500

    Awesome

---
+++ YouAreAwesome!/YouAreAwesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouAreAwesome!
+//
+//  Created by Jimmy Cassidy on 1/22/19.
+//  Copyright © 2019 Jimmy Cassidy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
