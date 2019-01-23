fdc172195d5eb67c57aa102f390b5fc3cc51ebda
Jimmy Cassidy <jimmycassidy@Jimmys-MacBook-Air.local>
2019-01-23 03:30:19 -0500

    DeveloperMe

---
+++ About Me/About Me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  About Me
+//
+//  Created by Jimmy Cassidy on 1/23/19.
+//  Copyright © 2019 Jimmy Cassidy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
