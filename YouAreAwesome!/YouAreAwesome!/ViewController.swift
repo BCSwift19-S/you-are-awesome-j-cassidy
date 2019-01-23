e3db332c04663e41504def4118da4207c6e21ded
Jimmy Cassidy <jimmycassidy@Jimmys-MacBook-Air.local>
2019-01-23 15:00:59 -0500

    2.7 Complete

--- YouAreAwesome!/YouAreAwesome!/ViewController.swift
+++ YouAreAwesome!/YouAreAwesome!/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var myMessage: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func myButtonPressed(_ sender: UIButton) {
+        let message1 = "You Are Awesome!"
+        let message2 = "You Are Great!"
+        let message3 = "You Are Amazing!"
+        
+        if myMessage.text == message1 {
+            myMessage.text = message2
+        } else if myMessage.text == message2 {
+            myMessage.text = message3
+        } else {
+            myMessage.text = message1
+        }
+    }
+    
 }
 
